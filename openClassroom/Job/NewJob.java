b40bbad0b2c9fc581783566435844c2dbccc970c
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-05-13 09:38:28 +0300

    creation of the job for training myself

---
+++ openClassroom/Job/NewJob.java
@@ -0,0 +1,5 @@
+package openClassroom.Job;
+
+public class NewJob {
+
+}
---
+++ openClassroom/Job/Worker.java
@@ -0,0 +1,12 @@
+package openClassroom.Job;
+
+public class Worker {
+	protected String firstName;
+	protected String lastName;
+	protected String idNumber;
+	public Worker(){
+		this.firstName = "Your First Name";
+		this.lastName = "Your Last Name";
+		this.idNumber = "ID000111";
+	}
+}
